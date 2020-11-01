4f0f37538ed1fabef1e66b2af11b16993c168617
Son Hoang Vu <son.hoang.vu@mgm-tp.com>
2020-11-01 23:40:10 +0700

    TLServer_Client-4: create utility for generating card image path.

---
+++ src/main/constant/Rank.java
@@ -0,0 +1,20 @@
+package main.constant;
+
+public class Rank {
+    public static final String ACE = "Ace";
+    public static final String TWO = "2";
+    public static final String THREE = "3";
+    public static final String FOUR = "4";
+    public static final String FIVE = "5";
+    public static final String SIX = "6";
+    public static final String SEVEN = "7";
+    public static final String EIGHT = "8";
+    public static final String NINE = "9";
+    public static final String TEN = "10";
+    public static final String JACK = "Jack";
+    public static final String QUEEN = "Queen";
+    public static final String KING = "King";
+
+    private Rank() {}
+
+}
